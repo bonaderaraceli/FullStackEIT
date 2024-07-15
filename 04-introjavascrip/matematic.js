1694b0e4c323265fe0c85376331ebba31a568260
aracelibonader <bonaderaraceli@gmail.com>
2024-07-15 11:08:30 -0300

    Intro a variables

---
+++ 04-introjavascrip/matematic.js
@@ -0,0 +1,23 @@
+//Operadores matematicos
+// + - * / %
+15 + 50
+
+let numUno= 15;
+let numDos= 50;
+
+/*resultado;
+let resultadoSuma;
+let resultadoResta;
+let resultadoMultiplicaciom;
+let resultadoDivision;*/
+
+resultado= numUno + numDos;
+resultado= numUno - numDos;
+//forma directa
+console.log (15+50);
+console.log (numUno + numDos);
+console.log (resultado);
+
+console.log('el resultado de la resta es:'resultado)
+
+alert('el resultado de la resta es:'resultado)
---
+++ 04-introjavascrip/variables.js
@@ -0,0 +1,67 @@
+//estos son los comentarios de una sola linea en java script
+/* comentarios
+entre 
+varias
+lineas en JS
+*/
+
+ //let hola =  pepe
+
+ /*
+ let hola =  pepe
+ let hola =  pepe
+ let hola =  pepe
+ let hola =  pepe
+ let hola =  pepe
+ let hola =  pepe
+*/
+console.log('3.Estoy en el archivo de JS')
+
+//variables: espacios de memoria para almacenar los datos
+//1. Con var
+//palabras reservadas: son utilizadas solo para las definiciones del lenguaje de programacion
+var nombre;//caracter/letra
+var email //numeros
+var edad;
+var pais
+//no
+var 12345;
+var nombre apellido;
+var= pais;
+var-edad
+//Si
+var $provincia;
+var_nombre;
+var nombreApellidoPersona; //camelcase
+var nombre_apellido; //underscare
+
+
+
+
+//2. Con let
+let provincia;
+
+
+
+//3. Const==> representa una constante: no cambia en la ejecucion del programa.
+//esto es una declaracion de una variable y asignacion de un valor
+const DNI= 12345678;
+// signo = representa asignacion de datos
+
+nombre= 'Juan';//string
+apellido= 'Perez'; 
+provincia= 'Buenos Aires';
+provincia='CABA'
+provincia='Cordoba';
+provincia= 15; //numeros - int
+provincia= true; //boolean
+provincia= false;
+
+//funciones por defecto
+console.log(nombre + apellido + provincia)
+console.info(nombre + apellido + provincia)
+console.error(nombre + apellido + provincia)
+console.warn(nombre + apellido + provincia)
+
+alert('soy un mensaje de alerta al usuario')
+//DNI= 'Pepe' //error en tiempo de ejecucion
