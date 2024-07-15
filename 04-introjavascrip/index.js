e94e4369dde412d5961485a8970a322ccec2863f
aracelibonader <bonaderaraceli@gmail.com>
2024-07-14 23:04:13 -0300

    intro a js

---
+++ 04-introjavascrip/index.js
@@ -0,0 +1,27 @@
+//estos son los comentarios de una sola linea en java script
+/* comentarios
+entre 
+varias
+lineas en JS
+*/
+
+ //let hola =  pepe
+
+ /*
+ let hola =  pepe
+ let hola =  pepe
+ let hola =  pepe
+ let hola =  pepe
+ let hola =  pepe
+ let hola =  pepe
+*/
+
+//variables: espacios de memoria para almacenar los datos
+//1. Con var
+//palabras reservadas: son utilizadas solo para las definiciones del lenguaje de programacion
+var nombre;
+var email
+var edad;
+var pais
+
+//2. Con let
