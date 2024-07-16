3c33931a466263b73de57af03582ac763a424a90
aracelibonader <bonaderaraceli@gmail.com>
2024-07-16 15:39:47 -0300

    alert y prompt listos

--- 04-introjavascript/matematic.js
+++ 04-introjavascript/matematic.js
@@ -5,19 +5,22 @@
 let numUno= 15;
 let numDos= 50;
 
+let resultado;
 /*resultado;
 let resultadoSuma;
 let resultadoResta;
-let resultadoMultiplicaciom;
+let resultadoMultiplicacion;
 let resultadoDivision;*/
 
-resultado= numUno + numDos;
 resultado= numUno - numDos;
 //forma directa
-console.log (15+50);
+console.log (15 + 50);
 console.log (numUno + numDos);
 console.log (resultado);
 
-console.log('el resultado de la resta es:'resultado)
+console.log('el resultado de la resta es:' + resultado)
 
-alert('el resultado de la resta es:'resultado)
+//alert('el resultado de la resta es:'resultado)
+
+
+
---
+++ 04-introjavascript/prompt.js
@@ -0,0 +1,11 @@
+//el usuario nos proporciona los valores de los numeros
+
+alert('Hola, vamos a sumar dos numeros!')
+
+//el propmt es una funcion que permite introducir al usuario un valor
+letnumeroUnoDelUsuario= prompt('ingresa el primer numero');
+console.log('el numero del user es' + numeroUnoDelUsuario );
+
+let confirmacion = confirm ('El numero que ingresaste es' + numeroUnoDelUsuario + ',¿es correcto?');
+
+console.log('La confirmacion del usuario es' + confirmacion)
