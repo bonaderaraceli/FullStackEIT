4e43049aae75f02d2b16ef5816159f29e6168871
aracelibonader <bonaderaraceli@gmail.com>
2024-08-08 17:50:09 -0300

    funciones JS

---
+++ 09-funciones/funciones.js
@@ -0,0 +1,95 @@
+//estructura de codigo que ejecuta una tarea especifica 
+//puede ser reutilizada en diferentes partes del codigo
+
+//son variables globales
+
+let password;
+let email;
+
+
+console.log('Hola Mundo');
+console.log('Hola Mundo');
+console.log('Hola Mundo');
+console.log('Hola Mundo');
+console.log('Hola Mundo');
+console.log('Hola Mundo');
+console.log('Hola Mundo');
+
+console.log('===================================');
+
+//estructura de una funcion sin parametros y/o argumentos /info que necesita la funcion para ejecutarse
+
+
+function saludoInicial () {console.log('Hola Mundo desde una Funcion')}
+
+saludoInicial();
+//las funciones no se ejecutan hasta que son llamadas/invocadas
+
+console.log('===================================');
+
+saludoInicial();
+saludoInicial();
+saludoInicial();
+saludoInicial();
+
+console.log('===================================');
+
+//funciones con parametros argumentos que necesita la funcion
+
+function sumar (numeroUno, numeroDos){
+    let resultado = numeroUno + numeroDos;
+    console.log (numeroUno);
+    console.log (numeroDos);
+    console.log('El resultado de la suma es:'+ resultado);
+}
+
+sumar(10,20);
+
+console.log('===================================');
+
+//variables locales: son las que viven dentro de una funcion y no pueden ser accedidos desde fuera de la funcion
+function sumarDos(x, y) {
+    let resultado= x + y;
+
+    console.log(x);
+    console.log(y);
+
+    console.log ('El resultado de la suma es:', resultado);
+}
+
+sumarDos(50,100);
+
+// console.log (x,y) error porque son variables locales
+console.log('===================================');
+
+let array = [1,2,3,4,5,6,7,8,9,10];
+for (let index=0; index < array.length; index++) {
+    const element= array [index];
+}
+
+//console.log (index);
+//error porque es una variable local
+
+console.log('===================================');
+//array funcion = funcion flecha
+// signo de igual + alt62 forman la flecha
+const restar = (numeroUno, numeroDos) => {
+    let resultado = numeroUno - numeroDos;
+    console.log (numeroUno);
+    console.log (numeroDos);
+    console.log('El resultado de la resta es:', resultado);
+};
+
+restar(100,50);
+
+console.log('===================================');
+
+
+
+
+
+
+
+
+
+
