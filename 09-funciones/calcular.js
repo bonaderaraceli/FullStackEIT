968c6d610779c0be163bdd2799bd0a98a3864164
aracelibonader <bonaderaraceli@gmail.com>
2024-08-18 17:41:00 -0300

    calculadora lista con la clase Math

--- 09-funciones/calcular.js
+++ 09-funciones/calcular.js
@@ -2,7 +2,22 @@
 const sumar = () => {
     //1.funcion parseINT: pasa un dato de tipo string a tipo entero
     let numUno= parseInt (prompt("Ingrese el primer numero:"));
+
+    console.log(typeof numUno);
+
+    if(edad == typeof Number){todo bien} else{la edad tiene que ser un numero}
+
+    //verificamos los datos
+    if(numUno == ' ' || is NaN(numUno) || numUno= null){
+        alert ('Ingrese un numero valido en el campo dado'); 
+        return;
+    }
+
     let numDos= parseInt(prompt("Ingrese el segundo numero:"));
+
+    if(numDos == ' ' || is NaN(numDos) || numDos= null){
+        alert ('Ingrese un numero valido en el campo dado'); 
+        return;
 
     let resultado= numUno + numDos;
 
@@ -11,9 +26,80 @@ const sumar = () => {
 
 const restar= () => {
     let numUno= parseInt(prompt ('Ingrese el primer numero'));
+
+    if(numUno == ' ' || is NaN(numUno) || numUno= null){
+        alert ('Ingrese un numero valido en el campo dado'); 
+        return; }
+
     let numDos= parseInt(prompt ('Ingrese el segundo numero'));
+
+    if(numDos == ' ' || is NaN(numDos) || numDos= null){
+        alert ('Ingrese un numero valido en el campo dado'); 
+        return;}
 
     let resultado= numUno - numDos;
 
     alert ('El resultado de la resta es:' - resultado);
 }
+
+const multiplicar= () => {
+    let numUno= parseInt(prompt ('Ingrese el primer numero'));
+    if(numUno == ' ' || is NaN(numUno) || numUno= null){
+        alert ('Ingrese un numero valido en el campo dado'); 
+        return; }
+
+    let numDos= parseInt (prompt ('Ingrese el segundo numero'));
+    if(numDos == ' ' || is NaN(numDos) || numDos= null){
+        alert ('Ingrese un numero valido en el campo dado'); 
+        return;}
+
+    let resultado= numUno * numDos;
+    console.log ('El resultado de la multiplicacion es:' + resultado);
+
+const dividir= () => {
+    let numUno= parseInt(prompt ('Ingrese el primer numero'));
+    if(numUno == ' ' || is NaN(numUno) || numUno= null || numUno= 0){
+        alert ('Ingrese un numero valido en el campo dado'); 
+        return; }
+
+    let numDos= parseInt (prompt ('Ingrese el segundo numero'));
+    if(numDos == ' ' || is NaN(numDos) || numDos= null){
+        alert ('Ingrese un numero valido en el campo dado'); 
+        return;}
+    
+    let resultado= numUno / numDos;
+    console.log('El resultado de la division es:' + resultado);
+
+// Objeto math para calculos avanzados
+// clase Math - Estatica :  no se puede instanciar
+console.log(Math.PI);
+console.log(Math.round (Math.random()* 100);
+console.log(Math.round (Math.random()* 100);
+console.log(Math.round (Math.random()* 100);
+console.log(Math.round (Math.random()* 100);
+
+console.log('=========================');
+Math.round(2.5);
+
+
+
+//let fiat= new auto (4, 'rojo', 'fiat', 'uno', 2000);
+//fiat.arrancar();
+
+//let Math= new Math(3,149494949);
+
+//Math.PI;
+
+
+
+
+//Ejemplo de funciones con Eventos nativos de JS
+//evento doble click
+function doble (){
+    alert('Doble click en el elemento');
+}
+//evento de teclado
+const contador = () => {
+    console.log('Presionaste restar');
+    console.log('windows');
+    }
