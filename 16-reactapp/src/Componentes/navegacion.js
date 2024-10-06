c09eb5140bccd0cdcc5fcd4482363fb10d2d7323
aracelibonader <bonaderaraceli@gmail.com>
2024-10-06 08:51:59 -0300

    primer componente en React

---
+++ 16-reactapp/src/Componentes/navegacion.js
@@ -0,0 +1,56 @@
+import { Link } from "react-router-dom";
+
+const Navegacion = () => {
+
+    // se necesita el método return para devolver UN SÓLO ELEMENTO JSX
+    return (
+        <nav className="navbar navbar-expand-lg bg-body-tertiary">
+            <div className="container-fluid">
+                <Link to="/" className="navbar-brand">APP</Link>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li className="nav-item">
+                            <Link to="/" className="nav-link active" aria-current="page">Home</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to="/login" className="nav-link">Login</Link>
+                        </li>
+                        <li className="nav-item dropdown">
+                            <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                Dropdown
+                            </a>
+                            <ul class="dropdown-menu">
+                                <li><a class="dropdown-item" href="#">Action</a></li>
+                                <li><a class="dropdown-item" href="#">Another action</a></li>
+                                <li>
+                                    <hr class="dropdown-divider"/>
+                                </li>
+                                <li><a class="dropdown-item" href="#">Something else here</a></li>
+                            </ul>
+                        </li>
+                        <li class="nav-item">
+                            <Link to="/nosotros" class="nav-link" aria-disabled="true">Nosotros</Link>
+                        </li>
+                    </ul>
+                    <form class="d-flex" role="search">
+                        <input 
+                            class="form-control me-2" 
+                            type="search" 
+                            placeholder="Search" 
+                            aria-label="Search"
+                        /> 
+                            <button class="btn btn-outline-success" type="submit">Search</button>
+                    </form>
+                </div>
+            </div>
+        </nav>
+    )
+
+};
+
+
+
+export default Navegacion;
