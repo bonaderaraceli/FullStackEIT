ef9773d44b1719792f8822385c8f29542fabb98d
aracelibonader <bonaderaraceli@gmail.com>
2024-10-07 19:20:31 -0300

    terminamos de configurar las rutas

---
+++ 16-reactapp/src/App.jsx
@@ -0,0 +1,43 @@
+// sintaxis para importar una imagen en react
+// import imagen from './logo.svg';
+
+// importar el archivo css
+import './css/App.css';
+
+// importamos el componente de navegación
+import Navegacion from './Componentes/Navegacion';
+import Login from './Pages/Login';
+import Nosotros from './Pages/Nosotros';
+import Home from './Pages/Home';
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+
+// primer componente de react - El componente Principal
+// componente funcional = el componente es una función
+const App = () => {
+
+  return ( 
+    
+    // JSX = JavaScript XML
+    // En los atributos de las etiquetas HTML para estilos usas camelCase
+
+    <React.Fragment>
+
+      <Navegacion />
+
+      <Routes>
+        <Route path="/" element={ <Home /> } />
+        <Route path="/login" element= { <Login />} />
+        <Route path="/nosotros" element= { <Nosotros />} />
+      </Routes>
+
+    </React.Fragment>
+
+  );
+
+}
+
+
+// exportar el componente App
+export default App;
